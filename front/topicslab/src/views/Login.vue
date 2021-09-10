34b22489184ca11d729e54e3412dfc7926d5c7a3
家田真帆 <idts-3620@yahoo.ne.jp>
2021-09-10 16:38:36 +0900

    make error message red

--- front/topicslab/src/views/Login.vue
+++ front/topicslab/src/views/Login.vue
@@ -15,7 +15,7 @@
             <InputText id="password" type="password" v-model="password" />
           </div>
         </div>
-        <span>{{message}}</span>
+        <span class="login-err-msg">{{message}}</span>
         <div class="p-field">
           <Button icon="pi pi-check" label="Login" v-on:click="login" />
         </div>
@@ -70,6 +70,10 @@ export default {
 .p-card-content {
   .fields {
     text-align: center;
+  }
+
+  .login-err-msg {
+  color: red;
   }
 
   .p-field {
